2840980aebe27aa3bbbd334361b7554680d2e328
mershter <100219272+mershter@users.noreply.github.com>
2022-05-09 13:39:23 +0200

    Jekyllifying this plus added responsive gallery
    Jekyllifying this plus added responsive gallery

---
+++ scripts.js
@@ -0,0 +1,10 @@
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+});
+
